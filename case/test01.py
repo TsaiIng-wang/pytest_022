f21ee8f35283dda89f13ebc9f0f1382b9b2c3ab3
TsaiIng-wang <1194555632@qq.com>
2020-04-02 18:09:13 +0800

    Initial commit

---
+++ case/test01.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_01():
+    print('执行测试用例01')
+
+def test_02():
+    print('执行测试用例02')
+
+def test_03():
+    print('执行测试用例03')
+
+if __name__ == '__main__':
+    pytest.main(['-s','test007.py'])
